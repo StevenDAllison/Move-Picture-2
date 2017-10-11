3ef8ad9ac839b57a22ccc4a4e8a2941adc1bbfff
steven.allison <steven.allison@CRO-ICG04-SA>
2017-10-11 10:47:30 +0100

    Now moves in a box

--- Move Picture 2/Form1.cs
+++ Move Picture 2/Form1.cs
@@ -21,7 +21,11 @@ namespace Move_Picture_2
         {
             pictureBox1.Location = new Point(pictureBox1.Location.X + 20, pictureBox1.Location.Y);
             await Task.Delay(500);
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + 20);
+            await Task.Delay(500);
             pictureBox1.Location = new Point(pictureBox1.Location.X - 20, pictureBox1.Location.Y);
+            await Task.Delay(500);
+            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - 20);
             await Task.Delay(500);
         }
     }
